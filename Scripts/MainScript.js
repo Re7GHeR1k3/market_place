2f3ef61caddb20c6a5b69effa031c3f73d00c564
alex0palii <saun2222222@gmail.com>
2023-02-22 15:02:05 +0200

    Test

--- Scripts/MainScript.js
+++ Scripts/MainScript.js
@@ -1,12 +1,14 @@
 let xhr = new XMLHttpRequest();
-let url = 'https://my-json-server.typicode.com/Re7GHeR1k3/market_place/';
+let url = 'https://my-json-server.typicode.com/Re7GHeR1k3/market_place';
 
-xhr.open('GET', url);
-xhr.responseType = 'text';
-xhr.send();
+xhr.open('GET',url + '/products');
+xhr.responseType = 'json'
+
 
 xhr.onload = function(){
 	const Text = xhr.response;
 	const TextR = JSON.parse(Text);
 	console.log(TextR);
 };
+
+xhr.send();
