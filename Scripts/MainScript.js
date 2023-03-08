6c6230d07268b1e48f6987ae1952985342af2686
Alex Palii <saun2222222@gmail.com>
2023-03-08 15:14:04 +0200

    first

--- Scripts/MainScript.js
+++ Scripts/MainScript.js
@@ -1,14 +1,23 @@
+let productsGrid = document.getElementById('products-grid')
+let productsArray = [];
 let xhr = new XMLHttpRequest();
 let url = 'https://my-json-server.typicode.com/Re7GHeR1k3/market_place';
 
-xhr.open('GET',url + '/products');
+xhr.open('GET', url + '/products');
 xhr.responseType = 'json'
-
-
 xhr.onload = function(){
-	const Text = xhr.response;
-	const TextR = JSON.parse(Text);
-	console.log(TextR);
+	let products = xhr.response;
+	productsGrid.innerHTML = null;
+	products.forEach(p => {
+		productsArray.push(p);
+		let pElem = document.createElement('div');
+		pElem.classList.add('product');
+		pElem.innerHTML = `
+			<h2>${p.name}</h2>
+		`;
+		productsGrid.append(pElem);
+	});
+
 };
 
 xhr.send();
