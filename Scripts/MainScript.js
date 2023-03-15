5ff10c49bc0879aed1aba87dcee06ac3de6ed22e
Alex Palii <saun2222222@gmail.com>
2023-03-15 15:14:22 +0200

    Seller

--- Scripts/MainScript.js
+++ Scripts/MainScript.js
@@ -14,7 +14,12 @@ xhr.onload = function(){
 		let pElem = document.createElement('div');
 		pElem.classList.add('product');
 		pElem.innerHTML = `
-			<h2>${p.name}</h2>
+			<h2 class="product-name">${p.name}</h2>
+			<img class="product-photo" src='${p.photo_url}' alt='${p.name}'>
+			<p class="product-price">${p.price}</p>
+			<p class="product-description"><b>Description: </b>${p.description}</p>
+			<a href=''>Seller profile</a>
+			<button>Buy</button>
 		`;
 		productsGrid.append(pElem);
 	});
